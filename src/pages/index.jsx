8a7bc7b9518b2a9045ca65e708fa8583a2eecfb9
JeasonCB <88633215+JeasonCB@users.noreply.github.com>
2022-08-27 13:31:15 -0400

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,44 +20,6 @@ export default function Home() {
         </Grid.Container> */}
         <Presentation />
       </Layout>
-      <Container gap={0} css={{ h: '100%' }}>
-        <Grid.Container>
-          <Grid css={{ bg: '$blue400', h: '100vh', w: '100%', userSelect: 'none' }}
-            as={motion.div}
-
-            initial={{ opacity: 0, y: -1 }}
-            whileInView={{
-              opacity: 1,
-              y: 1,
-              transition: {
-                type: 'spring',
-                bounce: 0.4,
-                duration: 1.55
-              }
-            }}
-            viewport={{ once: true }}
-          >
-            Blue
-          </Grid>
-          <Grid css={{ bg: '$gray100', h: '100vh', w: '100%', userSelect: 'none' }}
-            as={motion.div}
-
-            initial={{ opacity: 0, y: -1 }}
-            whileInView={{
-              opacity: 1,
-              y: -1,
-              transition: {
-                type: 'spring',
-                bounce: 0.4,
-                duration: 1.55
-              }
-            }}
-            viewport={{ once: true }}
-          >
-            gray
-          </Grid>
-        </Grid.Container>
-      </Container>
     </>
   )
 }
